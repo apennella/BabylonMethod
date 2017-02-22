d223282f483e90c66ab6a8fc3e92e95b7365be91
apennella <apennella@albany.edu>
2017-02-22 15:06:31 -0500

    Fixed some issues with conversion types

--- Babylon.java
+++ Babylon.java
@@ -4,28 +4,30 @@ public class Babylon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int guess;
+		double guess;
 		double number;
 		double error;
 		Scanner keyboard = new Scanner(System.in);
 		System.out.print("Please enter the number you wish to take the square root of");
 		number = keyboard.nextDouble();
-		System.out.println("Please enter you guess for the square root of that number");
-		guess = keyboard.nextInt();
+		guess = number/2;
 		System.out.println("Please enter your accaeptable percent error:");
 		error = keyboard.nextDouble();
-		System.out.println(error + " " + guess + " " + number);
 		Babylon babylon = new Babylon(guess, number, error);
+		System.out.println(error + " " + guess + " " + number);
 	}
-	int guess;
+	double guess;
 	double number;
 	double error;
-	public Babylon(int x, double y, double z){
+	public Babylon(double x, double y, double z){
 		guess= x;
 		number= y;
-		error = z;
+		error = z;	
 		System.out.println(error + " " + guess + " " + number);
 	}
-	
+	public double compute(){
+		
+		
+	}
 	}
 
